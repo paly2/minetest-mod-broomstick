0ed34af836d676a0daa5e3e300ba7b3ab1c85b45
palige <plangrognet@laposte.net>
2015-06-27 14:36:46 +0200

    add a file to revoke fly privilege to old users of broomsticks every start-up of the server

--- init.lua
+++ init.lua
@@ -4,6 +4,23 @@ local broomstick_actual_users = {}
 local had_fly_privilege = {}
 local privs = {}
 
+-- Remove fly to old users of broomstick
+users_file = io.open(minetest.get_modpath(minetest.get_current_modname()).."/broomstick_users.txt", "r")
+if users_file then
+	local string = users_file:read()
+	if string ~= nil then
+		broomstick_actual_users = minetest.deserialize(string)
+		for _, i in ipairs(broomstick_actual_users) do
+			privs = minetest.get_player_privs(i.name)
+			privs.fly = nil
+			minetest.set_player_privs(i.name, privs)
+		end
+	end
+	io.close(users_file)
+else
+	minetest.log("error", "[broomstick] Can not open broomstick_users.txt file !")
+end
+
 -- Register broomstick
 minetest.register_craftitem("broomstick:broomstick", {
 	description = "Broomstick",
@@ -33,13 +50,19 @@ minetest.register_craftitem("broomstick:broomstick", {
 				minetest.log("action", "Player " .. playername .. " has use a broomstick.")
 				-- Subtract mana...
 				mana.subtract(playername, broomstick_mana)
-				-- And insert player in the list.
+				-- Insert player in the list.
 				table.insert(broomstick_actual_users, {
 					name = playername,
 					time = 0,
 					is_warning_said = false
 				})
-				-- Remove broomstick...
+				-- Rewrite the broomstick_users.txt file.
+				users_file = io.open(minetest.get_modpath(minetest.get_current_modname()).."/broomstick_users.txt", "w")
+				if users_file then
+					users_file:write(minetest.serialize(broomstick_actual_users))
+					io.close(users_file)
+				end
+				-- Remove broomstick.
 				return ItemStack("")
 			else
 				minetest.chat_send_player(playername, "You already have a broomstick ! Please wait until the end of your actual broomstick.")
@@ -65,8 +88,14 @@ minetest.register_globalstep(function(dtime)
 			privs = minetest.get_player_privs(i.name)
 			privs["fly"] = nil
 			minetest.set_player_privs(i.name, privs)
-			-- And remove the player in the list.
+			-- Remove the player in the list.
 			table.remove(broomstick_actual_users, index)
+			-- Rewrite the broomstick_users.txt file.
+			users_file = io.open(minetest.get_modpath(minetest.get_current_modname()).."/broomstick_users.txt", "w")
+			if users_file then
+				users_file:write(minetest.serialize(broomstick_actual_users))
+				io.close(users_file)
+			end
 		end
 	end
 end)
@@ -76,5 +105,6 @@ minetest.register_craft({
 	output = "broomstick:broomstick",
 	recipe = {{"default:stick","default:stick","farming:wheat",}},
 })
+
 
 minetest.log("info", "[OK] broomstick")
