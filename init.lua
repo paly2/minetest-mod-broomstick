43ba6c38ab3468c0195f5a6cea122b11a81fe63e
palige <plangrognet@laposte.net>
2015-06-29 17:44:57 +0200

    Replace the minetest.register_globastep by a minetest.after

--- init.lua
+++ init.lua
@@ -53,8 +53,8 @@ minetest.register_craftitem("broomstick:broomstick", {
 		local playername = user:get_player_name()
 		if mana.get(playername) >= broomstick_mana then
 			local has_already_a_broomstick = false
-			for _, i in ipairs(broomstick_actual_users) do
-				if i.name == playername then
+			for i = 1, #broomstick_actual_users do
+				if broomstick_actual_users[i] == playername then
 					has_already_a_broomstick = true
 				end
 			end
@@ -77,11 +77,9 @@ minetest.register_craftitem("broomstick:broomstick", {
 				-- Subtract mana...
 				mana.subtract(playername, broomstick_mana)
 				-- Insert player in the list.
-				table.insert(broomstick_actual_users, {
-					name = playername,
-					time = 0,
-					is_warning_said = false
-				})
+				table.insert(broomstick_actual_users, playername)
+				-- And add the function in queue
+				minetest.after(broomstick_time-10, say_warning, playername)
 				-- Remove broomstick.
 				return ItemStack("")
 			else
@@ -94,33 +92,33 @@ minetest.register_craftitem("broomstick:broomstick", {
 })
 
 -- Broomstick timer
-minetest.register_globalstep(function(dtime)
-	for index, i in ipairs(broomstick_actual_users) do
-		i.time = i.time + dtime
-		-- Just a little warning message
-		if i.time >= broomstick_time - 10 and not i.is_warning_said then
-			minetest.chat_send_player(i.name, "WARNING ! You'll fall in 10 seconds !")
-			i.is_warning_said = true
-		elseif i.time >= broomstick_time then
-			-- Send a message...
-			minetest.chat_send_player(i.name, "End of broomstick. I hope you're not falling down...")
-			-- Set player privs...
-			privs = minetest.get_player_privs(i.name)
-			privs["fly"] = nil
-			minetest.set_player_privs(i.name, privs)
-			-- Remove the player in the list.
-			table.remove(broomstick_actual_users, index)
-			-- Rewrite the broomstick_users.txt file.
-			had_fly_privilege[i.name] = nil
-			save()
+local function say_warning(playername)
+	minetest.chat_send_player(playername, "WARNING ! You'll fall in 10 seconds !")
+	minetest.after(10, end_broomstick, playername)
+end
+
+local function end_broomstick(playername)
+	-- Send a message...
+	minetest.chat_send_player(playername, "End of broomstick. I hope you're not falling down...")
+	-- Set player privs...
+	privs = minetest.get_player_privs(playername)
+	privs["fly"] = nil
+	minetest.set_player_privs(playername, privs)
+	-- Remove the player in the list.
+	for i = 1, #broomstick_actual_users do
+		if broomstick_actual_users[i] == playername then
+			table.remove(broomstick_actual_users, i)
 		end
 	end
-end)
+	-- Rewrite the broomstick_users.txt file.
+	had_fly_privilege[playername] = nil
+	save()
+end
 
 -- Craft
 minetest.register_craft({
 	output = "broomstick:broomstick",
-	recipe = {{"default:stick","default:stick","farming:wheat",}},
+	recipe = {{"default:stick","default:stick","",}},
 })
 
 
